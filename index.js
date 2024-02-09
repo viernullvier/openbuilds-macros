7f976930d051ffaaae9df3070404673d93226f68
viernullvier <1492179+viernullvier@users.noreply.github.com>
2024-02-09 23:55:15 +0100

    feat(build): Only git-add JSON files for staged source files

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { readdir, readFile, writeFile } from 'fs/promises'
 import { join } from 'path'
+import { exec as cbExec } from 'child_process'
+import { promisify } from  'util'
+
+const exec = promisify(cbExec)
 
 const dirs = (await readdir('.', {withFileTypes: true})).filter(_ => _.isDirectory()).map(_ => _.name)
 await Promise.all(dirs.map(async dir => {
@@ -8,15 +12,23 @@ await Promise.all(dirs.map(async dir => {
   if (!content.find(_ => _.isFile() && _.name === 'index.js')) return
   if (!content.find(_ => _.isFile() && _.name === `${dir}.json`)) return
   const jsonPath = join(path, `${dir}.json`)
+  const sourcePath = join(path, 'index.js')
   console.log(`Building ${dir}…`)
   const infile = await readFile(jsonPath)
-  const source = await readFile(join(path, 'index.js'), {encoding: 'utf8'})
+  const source = await readFile(sourcePath, {encoding: 'utf8'})
   try {
     const jsonContent = JSON.parse(infile)
     jsonContent['javascript'] = source
     await writeFile(jsonPath, JSON.stringify(jsonContent, undefined, 2))
     console.log(`Build for ${dir} finished.`)
+
+    try {
+      const result = await exec(`git diff --staged --exit-code ${sourcePath}`)
+    } catch {
+      await exec(`git add ${jsonPath}`)
+    }
   } catch (e) {
     console.error(`Error building ${dir}: ${e.message}`)
+    process.exit(1)
   }
 }))
